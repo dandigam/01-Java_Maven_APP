2212b15e53b7121ddb212d1ebd6460c8e21e335e
dandigam <dandigam@gail.com>
2022-06-29 07:48:35 +0530

    update msg to oracle

--- src/main/java/com/dandigam/App.java
+++ src/main/java/com/dandigam/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Oracle!" );
+		 System.out.println( "Hello Oracle!" );
+		  System.out.println( "Hello Oracle!" );
+		   System.out.println( "Hello Oracle!" );
+		    System.out.println( "Hello Oracle!" );
+			 System.out.println( "Hello Oracle!" );
+			  System.out.println( "Hello Oracle!" );
+			   System.out.println( "Hello Oracle!" );
+			    System.out.println( "Hello Oracle!" );
     }
 }
